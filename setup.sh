6ddec1a394c7f7559b990933abf1986bd3f431a3
ABOL_HACKER <abolfazlm3419.2@gmail.com>
2024-07-15 20:55:11 +0330

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Function to update the repository
+update_repository() {
+    echo "Updating repository..."
+    bash update.sh
+    echo "Repository updated successfully!"
+}
+
+# Main script
+clear
+echo
+pkg install pv -y >/dev/null 2>&1
+echo -e "\033[32m\033[1m{───────────────────────────────────────────────────}"
+echo -e "\033[33m\033[1m   Installing All Required Packages! Please Wait..." | pv -qL 10
+apt update                    
+apt upgrade -y 
+pkg install cmatrix -y 
+pkg install pv -y 
+apt install figlet -y  
+apt install ruby -y 
+apt install mpv -y 
+echo -e "\033[31m\033[1m        INSTALLATION COMPLETED \033[32m[\033[36m✓\033[32m]" | pv -qL 12
+echo -e "\033[33m\033[1m]────────────────────────────────────────────["
+termux-setup-storage
+cd $HOME/ABOLHACKER
+
+# Prompt user for update
+read -p "Do you want to update the repository now? (y/n): " choice
+if [ "$choice" = "y" ]; then
+    update_repository
+fi
+
+# Run login script
+bash termux.sh
+
