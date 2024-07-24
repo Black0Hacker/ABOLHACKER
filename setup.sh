41153f72a8e3d3d9daab51d577db7fc087d16b4c
ABOL_HACKER <abolfazlm3419.2@gmail.com>
2024-07-24 16:24:39 +0330

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
+# Function to update the repository
+update_repository() {
+    echo "Updating repository..."
+    bash update.sh
+    echo "Repository updated successfully!"
+}
+
 # Main script
 clear
 echo
 pkg install pv -y >/dev/null 2>&1
+echo -e "\033[32m\033[1m{───────────────────────────────────────────────────}"
+echo -e "\033[33m\033[1m   Installing All Required Packages! Please Wait..." | pv -qL 10
 apt update                    
 apt upgrade -y 
 pkg install bash
 pkg install pv -y 
 apt install figlet -y  
+gem install lolcat
 echo -e "\033[31m\033[1m        INSTALLATION COMPLETED \033[32m[\033[36m✓\033[32m]" | pv -qL 12
+echo -e "\033[33m\033[1m]────────────────────────────────────────────["
 termux-setup-storage
 cd $HOME/ABOLHACKER
 bash banner.sh
